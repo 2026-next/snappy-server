67a06becafe3555545b253ef3a949dee697570ea
Jaemin Kim <jeffthehero038@gmail.com>
2026-04-09 16:08:09 +0900

    Swagger Init

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Snappy Server')
+    .setDescription('API documentation for the Snappy server')
+    .setVersion('1.0')
+    .build();
+
+  const document = () => SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document());
+
   const port = Number(process.env.PORT || 3000);
   await app.listen(port, '0.0.0.0');
 }
