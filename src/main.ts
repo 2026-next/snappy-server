2acd37a60e4f1a1029de6538ca830ea013944db6
Jaemin Kim <jeffthehero038@gmail.com>
2026-05-07 08:10:30 +0000

    CORS fixed

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,14 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: [
+      process.env.FRONTEND_ORIGIN,
+      process.env.LOCALHOST_ORIGIN,
+    ],
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
